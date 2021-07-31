ab75076e612358b07a0625eba14811a0542682b2
Ericboo <eric43656@gmail.com>
2021-07-31 09:15:34 -0300

    Added new pages and models

---
+++ lib/models/aluno.dart
@@ -0,0 +1,11 @@
+class Aluno {
+
+  final String nome;
+  final String email;
+  final String senha;
+  double? nota1;
+  double? nota2;
+
+  Aluno(this.nome, this.email, this.senha, {this.nota1, this.nota2});
+
+}
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,19 +1,56 @@
+import 'package:eldoom/pages/dashboard/dashboard.dart';
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+//Login é a pagina de abertura do aplicativo. O professor ou aluno irá inserir
+//suas credenciais e o aplicativo irá validar num banco de dados;
 
+//TODO: implementar uma web api.
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+
+  bool isChecked = false;
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Center(child: Text('Eldoom'),),),
+      appBar: AppBar(
+        title: Center(
+          child: Text('Eldoom'),
+        ),
+      ),
       body: Column(
         children: [
           LoginInput('Email', Icons.person, false),
           LoginInput('Senha', Icons.lock, true),
           Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Row(
+              children: [
+                Checkbox(
+                  checkColor: Colors.white,
+                  fillColor: MaterialStateProperty.resolveWith((state) => Colors.blue),
+                  value: isChecked,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      isChecked = value!;
+                    });
+                  },
+                ),
+                Text('Lembre-se de mim', style: TextStyle(color: Theme.of(context).primaryColor, fontSize: 16),),
+              ],
+            ),
+          ),//Checkbox
+          Padding(
             padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12),
             child: InkWell(
-              onTap: () {},
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Dashboard()));
+              },
               child: Container(
                 decoration: BoxDecoration(
                   color: Theme.of(context).primaryColor,
@@ -21,16 +58,21 @@ class Login extends StatelessWidget {
                 ),
                 width: double.infinity,
                 height: 50,
-                child: Center(child: Text('Login', style: TextStyle(color: Colors.white),)),
+                child: Center(
+                    child: Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white),
+                )),
               ),
             ),
-          ),
-          Text('Eldoom é uma plataforma fictícia gratuita feita para o gerenciamento '
-              'coordenado de uma turma.',
+          ),//Button
+          Text(
+            'Eldoom é uma plataforma fictícia gratuita feita para o gerenciamento '
+            'coordenado de uma turma.',
             textAlign: TextAlign.center,
             style: TextStyle(
-                color: Theme.of(context).primaryColor,
-                fontSize: 20,
+              color: Theme.of(context).primaryColor,
+              fontSize: 20,
             ),
           ),
         ],
@@ -40,10 +82,10 @@ class Login extends StatelessWidget {
 }
 
 class LoginInput extends StatelessWidget {
-
   final String label;
   final IconData icon;
   final bool isObscure;
+
   LoginInput(this.label, this.icon, this.isObscure);
 
   @override
@@ -56,12 +98,16 @@ class LoginInput extends StatelessWidget {
           borderRadius: BorderRadius.circular(12),
         ),
         child: TextFormField(
-          keyboardType: isObscure ? TextInputType.text: TextInputType.emailAddress,
+          keyboardType:
+              isObscure ? TextInputType.text : TextInputType.emailAddress,
           obscureText: isObscure,
           style: TextStyle(color: Colors.white, fontSize: 20),
           decoration: InputDecoration(
             border: InputBorder.none,
-            icon: Icon(icon, color: Theme.of(context).primaryColor,),
+            icon: Icon(
+              icon,
+              color: Theme.of(context).primaryColor,
+            ),
             hintText: label,
           ),
         ),
